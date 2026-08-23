22278e669755c013443d4ea6408a21534964831a
JONSU-AG <aguilar.jonsu@gmail.com>
2026-08-22 22:51:04 -0500

    Add files via upload

---
+++ assets/briceñoareas2027.js
@@ -0,0 +1,111 @@
+  window.briceno2027Areas = [
+    {
+      "nombre": "BIOLOGIA",
+      "videos": [
+        {
+          "nombre": "S01 BIOLOGIA 17-08 VER",
+          "url": "https://youtu.be/q1n4eA1xfrU"
+        },
+      {
+        "nombre": "S01 ANATOMIA 18-08 VER",
+        "url": "https://youtu.be/tQ12TTcowt0"
+      }
+    ]
+  },
+  {
+    "nombre": "MATEMÁTICA",
+    "videos": [
+      {
+        "nombre": "S00 MATEMATICA 17-08 VER",
+        "url": "https://youtu.be/uOTjoNwo808"
+      },
+      {
+        "nombre": "MATEMATICA 19-08 VER",
+        "url": "https://youtu.be/sJq9_nDBwfI"
+      }
+    ]
+  },
+  {
+    "nombre": "QUIMICA",
+    "videos": [
+      {
+        "nombre": "QUIMICA 20-08 VER",
+        "url": "https://youtu.be/yQf4FjDFC4Y"
+      }
+    ]
+  },
+  {
+    "nombre": "LENGUAJE",
+    "videos": [
+      {
+        "nombre": "LENGUAJE 19-08 VER",
+        "url": "https://youtu.be/pQ1XJvxOvWQ"
+      },
+      {
+        "nombre": "LENGUAJE 20-08 VER",
+        "url": "https://youtu.be/xDLfw1o6nzw"
+      }
+    ]
+  },
+  {
+    "nombre": "RAZ. VERBAL",
+    "videos": [
+      {
+        "nombre": "S00 RAZ. VERBAL 18-09 VER",
+        "url": "https://youtu.be/HHGwqK9vnhg"
+      },
+      {
+        "nombre": "CLASES GRABADAS LUNES 17-08 VER",
+        "url": "https://youtu.be/btJHN9rsVdI"
+      }
+    ]
+  },
+  {
+    "nombre": "RAZ. MATEMÁTICO",
+    "videos": [
+      {
+        "nombre": "S00 RAZ. MATEMATICO 18-08 VER",
+        "url": "https://youtu.be/rftlHAiyEIo"
+      },
+      {
+        "nombre": "RAZ.MATEMATICO 17-08 VER",
+        "url": "https://youtu.be/CM4yCO3TnFo"
+      },
+      {
+        "nombre": "RAZ. MATEMATICO 19-08 VER",
+        "url": "https://youtu.be/-ffPZ3x-knw"
+      }
+    ]
+  },
+  {
+    "nombre": "HISTORIA",
+    "videos": [
+      {
+        "nombre": "HISTORIA 19-08 VER",
+        "url": "https://youtu.be/5XF6ICfnv60"
+      }
+    ]
+  },
+  {
+    "nombre": "GEOGRAFIA",
+    "videos": [
+      {
+        "nombre": "S01 GEOGRAFIA 18-09 VER",
+        "url": "https://youtu.be/jAp53THHgVM"
+      },
+      {
+        "nombre": "GEOGRAFIA 20-08 VER",
+        "url": "https://youtu.be/mAb101fpvD0"
+      }
+    ]
+  },
+  {
+    "nombre": "FISICA",
+    "videos": [
+      {
+        "nombre": "FISICA 20-08 VER",
+        "url": "https://youtu.be/fqeQxm8R4sk"
+      }
+    ]
+  }
+];
