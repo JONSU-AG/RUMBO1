ddcdadb4a179e1cf54227581dfb16bfadd15463d
JONSU-AG <aguilar.jonsu@gmail.com>
2026-08-26 11:12:41 -0500

    Refactor briceñoareas2027.js to add weekly data

--- assets/briceñoareas2027.js
+++ assets/briceñoareas2027.js
@@ -1,18 +1,28 @@
-  window.briceno2027Areas = [
-    {
-      "nombre": "BIOLOGIA",
-      "videos": [
-        {
-          "nombre": "S01 BIOLOGIA 17-08 VER",
-          "url": "https://youtu.be/q1n4eA1xfrU"
-        },
+// ============================================================
+// DATOS BRICEÑO 2027 POR SEMANAS
+// ============================================================
+
+window.briceno2027Semana00 = [
+  {
+    "categoria": "CIENCIAS ONLINE MAÑANA",
+    "nombre": "BIOLOGIA",
+    "videos": [
+      {
+        "nombre": "S01 BIOLOGIA 17-08 VER",
+        "url": "https://youtu.be/q1n4eA1xfrU"
+      },
       {
         "nombre": "S01 ANATOMIA 18-08 VER",
         "url": "https://youtu.be/tQ12TTcowt0"
+      },
+      {
+        "nombre": "S00 BIOLOGIA 21-08 VER",
+        "url": "https://youtu.be/SbKRuRlAsn4"
       }
     ]
   },
   {
+    "categoria": "CIENCIAS ONLINE MAÑANA",
     "nombre": "MATEMÁTICA",
     "videos": [
       {
@@ -22,19 +32,33 @@
       {
         "nombre": "MATEMATICA 19-08 VER",
         "url": "https://youtu.be/sJq9_nDBwfI"
+      },
+      {
+        "nombre": "MATEMATICA 22-08 VER",
+        "url": "https://youtu.be/0UZQcDr1axI"
       }
     ]
   },
   {
+    "categoria": "CIENCIAS ONLINE MAÑANA",
     "nombre": "QUIMICA",
     "videos": [
       {
         "nombre": "QUIMICA 20-08 VER",
         "url": "https://youtu.be/yQf4FjDFC4Y"
+      },
+      {
+        "nombre": "QUIMICA 21-08 VER",
+        "url": "https://youtu.be/g9gZJ_pj63c"
+      },
+      {
+        "nombre": "QUIMICA 22-08 VER",
+        "url": "https://youtu.be/ceSgbWyRi7w"
       }
     ]
   },
   {
+    "categoria": "CIENCIAS ONLINE MAÑANA",
     "nombre": "LENGUAJE",
     "videos": [
       {
@@ -48,28 +72,30 @@
     ]
   },
   {
+    "categoria": "CIENCIAS ONLINE MAÑANA",
     "nombre": "RAZ. VERBAL",
     "videos": [
       {
-        "nombre": "S00 RAZ. VERBAL 18-09 VER",
-        "url": "https://youtu.be/HHGwqK9vnhg"
-      },
-      {
         "nombre": "CLASES GRABADAS LUNES 17-08 VER",
         "url": "https://youtu.be/btJHN9rsVdI"
+      },
+      {
+        "nombre": "S00 RAZ. VERBAL 18-09 VER",
+        "url": "https://youtu.be/HHGwqK9vnhg"
       }
     ]
   },
   {
+    "categoria": "CIENCIAS ONLINE MAÑANA",
     "nombre": "RAZ. MATEMÁTICO",
     "videos": [
       {
-        "nombre": "S00 RAZ. MATEMATICO 18-08 VER",
-        "url": "https://youtu.be/rftlHAiyEIo"
-      },
-      {
         "nombre": "RAZ.MATEMATICO 17-08 VER",
         "url": "https://youtu.be/CM4yCO3TnFo"
+      },
+      {
+        "nombre": "S00 RAZ. MATEMATICO 18-08 VER",
+        "url": "https://youtu.be/rftlHAiyEIo"
       },
       {
         "nombre": "RAZ. MATEMATICO 19-08 VER",
@@ -78,15 +104,41 @@
     ]
   },
   {
+    "categoria": "CIENCIAS ONLINE MAÑANA",
+    "nombre": "PSICOLOGÍA",
+    "videos": [
+      {
+        "nombre": "PSICOLOGIA 21-08 VER",
+        "url": "https://youtu.be/uXwuZcENbGc"
+      }
+    ]
+  },
+  {
+    "categoria": "CIENCIAS ONLINE MAÑANA",
+    "nombre": "LITERATURA",
+    "videos": [
+      {
+        "nombre": "LITERATURA 22-08 VER",
+        "url": "https://youtu.be/I8auhe92-ss"
+      }
+    ]
+  },
+  {
+    "categoria": "CIENCIAS ONLINE MAÑANA",
     "nombre": "HISTORIA",
     "videos": [
       {
         "nombre": "HISTORIA 19-08 VER",
         "url": "https://youtu.be/5XF6ICfnv60"
+      },
+      {
+        "nombre": "HISTORIA 22-08 VER",
+        "url": "https://youtu.be/zEFSJ6HmHvM"
       }
     ]
   },
   {
+    "categoria": "CIENCIAS ONLINE MAÑANA",
     "nombre": "GEOGRAFIA",
     "videos": [
       {
@@ -100,12 +152,147 @@
     ]
   },
   {
+    "categoria": "CIENCIAS ONLINE MAÑANA",
     "nombre": "FISICA",
     "videos": [
       {
         "nombre": "FISICA 20-08 VER",
         "url": "https://youtu.be/fqeQxm8R4sk"
+      },
+      {
+        "nombre": "FISICA 21-08 VER",
+        "url": "https://youtu.be/XA9s7OIaAIg"
       }
     ]
   }
 ];
+
+window.briceno2027Semana01 = [
+  {
+    "categoria": "CIENCIAS ONLINE MAÑANA",
+    "nombre": "BIOLOGIA",
+    "videos": [
+      {
+        "nombre": "S01 BIOLOGIA 24-08 VER",
+        "url": "https://youtu.be/fymH_4ARmxw"
+      },
+      {
+        "nombre": "S01 ANATOMIA 25-08 VER",
+        "url": "https://youtu.be/TIlIbbK9EWY"
+      }
+    ]
+  },
+  {
+    "categoria": "CIENCIAS ONLINE MAÑANA",
+    "nombre": "MATEMÁTICA",
+    "videos": [
+      {
+        "nombre": "S01 MATEMATICA 24-08 VER",
+        "url": "https://youtu.be/Rn0xHKjEga0"
+      }
+    ]
+  },
+  {
+    "categoria": "CIENCIAS ONLINE MAÑANA",
+    "nombre": "RAZ. VERBAL",
+    "videos": [
+      {
+        "nombre": "RAZ. VERBAL 24-08 VER",
+        "url": "https://youtu.be/ylus47BV4yc"
+      },
+      {
+        "nombre": "RAZ. VERBAL 25-08 VER",
+        "url": "https://youtu.be/0bypXh-FgRk"
+      }
+    ]
+  },
+  {
+    "categoria": "CIENCIAS ONLINE MAÑANA",
+    "nombre": "RAZ. MATEMÁTICO",
+    "videos": [
+      {
+        "nombre": "RAZ. MATEMATICO 24-08 VER",
+        "url": "https://youtu.be/Lqdw5R_QMW0"
+      },
+      {
+        "nombre": "RAZ. MATEMATICO 25-08 VER",
+        "url": "https://youtu.be/cvDr_U9wtjI"
+      }
+    ]
+  },
+  {
+    "categoria": "CIENCIAS ONLINE MAÑANA",
+    "nombre": "GEOGRAFIA",
+    "videos": [
+      {
+        "nombre": "S01 GEOGRAFIA 25-08 VER",
+        "url": "https://youtu.be/3ZQ3MohxVEY"
+      }
+    ]
+  },
+  {
+    "categoria": "SEMINARIOS DE CIENCIAS",
+    "nombre": "CIVICA",
+    "videos": [
+      {
+        "nombre": "S01 - SEMINARIO DE CÍVICA PARA CIENCIAS 24-08 VER",
+        "url": "https://youtu.be/CJt6s_oWMDo"
+      }
+    ]
+  },
+  {
+    "categoria": "SEMINARIOS DE CIENCIAS",
+    "nombre": "ANATOMIA",
+    "videos": [
+      {
+        "nombre": "S01 - SEMINARIO DE ANATOMIA PARA BIOMEDICAS 24-08 VER",
+        "url": "https://youtu.be/BDojn-bPROE"
+      },
+      {
+        "nombre": "S01 - SEMINARIO DE ANATOMIA PARA CIENCIAS 25-08 VER",
+        "url": "https://youtu.be/8ekB7R7hnTA"
+      }
+    ]
+  },
+  {
+    "categoria": "SEMINARIOS DE CIENCIAS",
+    "nombre": "BIOLOGIA",
+    "videos": [
+      {
+        "nombre": "S01 - SEMINARIO DE BIOLOGIA PARA BIOMEDICAS 25-08 VER",
+        "url": "https://youtu.be/EyoMQ49QIEs"
+      }
+    ]
+  },
+  {
+    "categoria": "SEMINARIOS DE CIENCIAS",
+    "nombre": "RAZONAMIENTO MATEMÁTICO",
+    "videos": [
+      {
+        "nombre": "S01 - SEMINARIO DE RAZONAMIENTO MATEMÁTICO PARA INGENIERÍAS 24-08 VER",
+        "url": "https://youtu.be/NKeZuKM2Z7I"
+      }
+    ]
+  },
+  {
+    "categoria": "SEMINARIOS DE CIENCIAS",
+    "nombre": "FILOSOFIA",
+    "videos": [
+      {
+        "nombre": "S01 - SEMINARIO DE FILOSOFIA PARA CIENCIAS 25-08 VER",
+        "url": "https://youtu.be/nBnNGzEXPic"
+      }
+    ]
+  },
+  {
+    "categoria": "SEMINARIOS DE CIENCIAS",
+    "nombre": "MATEMÁTICA",
+    "videos": [
+      {
+        "nombre": "S01 - SEMINARIO DE MATEMÁTICA PARA INGENIERÍAS 25-08 VER",
+        "url": "https://youtu.be/-wpz5soYQLk"
+      }
+    ]
+  }
+];
+
